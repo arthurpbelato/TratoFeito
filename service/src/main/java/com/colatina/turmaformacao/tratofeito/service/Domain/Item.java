fbe4ee277c0da36bf691df0e353984447fc58041
arthurpbelato <belatoarthur@outloo.com>
2021-04-29 20:16:37 -0300

    fix imports

--- service/src/main/java/com/colatina/turmaformacao/tratofeito/service/Domain/Item.java
+++ service/src/main/java/com/colatina/turmaformacao/tratofeito/service/Domain/Item.java
@@ -3,7 +3,17 @@ package com.colatina.turmaformacao.tratofeito.service.Domain;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 import java.io.Serializable;
 
 @Getter
--- service/src/main/java/com/colatina/turmaformacao/tratofeito/service/Domain/Usuario.java
+++ service/src/main/java/com/colatina/turmaformacao/tratofeito/service/Domain/Usuario.java
@@ -3,7 +3,13 @@ package com.colatina.turmaformacao.tratofeito.service.Domain;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 import java.io.Serializable;
 import java.time.LocalDate;
 
