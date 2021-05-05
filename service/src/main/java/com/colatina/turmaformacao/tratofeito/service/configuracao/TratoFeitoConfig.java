676c958f51788b12986f095aad5c4dca2c21d4b3
nyckols <nicholas.ifes@outlook.com>
2021-05-05 20:34:25 -0300

    configuracao de email 3

--- service/src/main/java/com/colatina/turmaformacao/tratofeito/service/configuracao/TratoFeitoConfig.java
+++ service/src/main/java/com/colatina/turmaformacao/tratofeito/service/configuracao/TratoFeitoConfig.java
@@ -1,8 +1,5 @@
 package com.colatina.turmaformacao.tratofeito.service.configuracao;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
