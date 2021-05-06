0197e9701788c6111fce39bad50903302ee8c734
Aleahpar <raphaelabarreto96@gmail.com>
2021-05-06 15:23:22 -0300

    regras de negocio e fiz nos dtos

--- service/src/main/java/com/colatina/turmaformacao/tratofeito/service/servico/dto/OfertaListagemDTO.java
+++ service/src/main/java/com/colatina/turmaformacao/tratofeito/service/servico/dto/OfertaListagemDTO.java
@@ -1,12 +1,16 @@
 package com.colatina.turmaformacao.tratofeito.service.servico.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class OfertaListagemDTO {
-    private long id;
+    private Long id;
     private String nomeItem;
     private String nomeUsuario;
     private String situacao;
