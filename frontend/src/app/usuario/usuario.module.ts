5e97a69f82d82c4bd31357a505b4c43e995e8003
arthurpbelato <belatoarthur@outlook.com>
2021-05-18 21:10:58 -0300

    removendo listagem

--- frontend/src/app/usuario/usuario.module.ts
+++ frontend/src/app/usuario/usuario.module.ts
@@ -3,13 +3,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UsuarioRoutingModule } from './usuario-routing.module';
-import { ListagemPageComponent } from './listagem-page/listagem-page.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ProfilePageComponent } from './profile-page/profile-page.component';
 
 
 @NgModule({
-  declarations: [ListagemPageComponent, ProfilePageComponent],
+  declarations: [ProfilePageComponent],
   imports: [
     CommonModule,
     UsuarioRoutingModule,
