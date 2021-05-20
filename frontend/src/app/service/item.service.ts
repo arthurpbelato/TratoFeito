fe74ae3f5d5ee71b84eec6a4bb1115ef60e3d8b2
nyckols <nicholas.ifes@outlook.com>
2021-05-19 21:57:48 -0300

    arquivo item-servico

--- frontend/src/app/service/item.service.ts
+++ frontend/src/app/service/item.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { ItemModel } from '../model/item.model';
+import { ItemModel } from '../item/model/item.model';
 
 @Injectable({
   providedIn: 'root'
