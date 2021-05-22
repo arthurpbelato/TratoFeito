9539d1c4dbbb770c7e87821b229d30e699842c81
Aleahpar <raphaelabarreto96@gmail.com>
2021-05-22 00:47:28 -0300

    homepage

---
+++ service/src/main/java/com/colatina/turmaformacao/tratofeito/service/servico/dto/ItemIdListDTO.java
@@ -0,0 +1,2 @@
+package com.colatina.turmaformacao.tratofeito.service.servico.dto;public class ItemIdListDTO {
+}
---
+++ service/src/main/java/com/colatina/turmaformacao/tratofeito/service/servico/mapper/OfertaDetalhadaMapper.java
@@ -0,0 +1,2 @@
+package com.colatina.turmaformacao.tratofeito.service.servico.mapper;public interface OfertaDetalhadaMapper {
+}
